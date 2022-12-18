dceef3b11a1f42512be9a5889cb3710aba29fcce
illyge <ilia.vishnevsky@gmail.com>
2022-12-18 13:44:21 +0100

    refactoring, grooming

---
+++ pipeline_util.py
@@ -0,0 +1,85 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.decomposition import TruncatedSVD
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.compose import ColumnTransformer
+from sklearn.feature_selection import SelectKBest, chi2
+
+def kw_to_dict(series):
+    return [{'keyword': item[1]} for item in series.items()]
+
+def loc_to_dict(series):
+    return [{'location': item[1]} for item in series.items()]
+
+def make_kw_pipeline():
+    return Pipeline(steps=[('kw_to_dict', FunctionTransformer(kw_to_dict)),
+                              ('d_vect', DictVectorizer())])
+def make_loc_pipeline(): 
+    return Pipeline(steps=[('kw_to_dict', FunctionTransformer(loc_to_dict)),
+                              ('d_vect', DictVectorizer())])
+
+def make_poly2_k_best_pipeline():
+    return Pipeline(steps=[('poly2', PolynomialFeatures(2)),
+                                        ('k_best', SelectKBest(chi2))])
+
+def make_vectorizer(
+        min_df=10, 
+        kw=False, 
+        loc=False, 
+        hashtags=False,
+        urls=False,
+        stopwords=None,
+        preprocessor=None,
+        ngram_range=(1, 1)):
+    columns = [('text_c_vect', CountVectorizer(min_df=min_df, stop_words=stopwords, preprocessor=preprocessor, ngram_range=ngram_range), 'text')]
+    if kw:
+        columns.append(('kw_dict_vect', make_kw_pipeline(), 'keyword'))
+    if loc:
+        columns.append(('loc_dict_vect', make_loc_pipeline(), 'location'))
+    if hashtags:
+        columns.append(('hashtags_c_vect', CountVectorizer(min_df=5, analyzer=lambda x: x), 'hashtags'))
+    if urls:
+        columns.append(('urls_c_vect', CountVectorizer(min_df=3, analyzer=lambda x: x), 'urls'))        
+    
+    return ColumnTransformer(columns)
+    
+
+def make_preparation_pipeline(
+        url_cleaner=None,
+        min_df=10, 
+        kw=False, 
+        loc=False, 
+        hashtags=False,
+        urls=False,
+        stopwords=None,
+        preprocessor=None,
+        ngram_range=(1, 1),
+    ):
+    
+    vectorizer=make_vectorizer(
+        min_df=min_df,
+        kw=kw, 
+        loc=loc, 
+        hashtags=hashtags,
+        stopwords=stopwords,
+        preprocessor=preprocessor,
+        ngram_range=ngram_range)
+    
+    steps = [('url_cleaner', url_cleaner)]    
+    steps.append(('vectorizer', vectorizer))
+    
+    return Pipeline(steps=steps)
+
+def make_transformation_pipeline(
+    classifier,
+    params
+):
+    pipeline = make_preparation_pipeline(**params) 
+    steps = pipeline.steps.copy()
+    steps.append(('poly2_k_best', make_poly2_k_best_pipeline()))
+    steps.append(('svd', TruncatedSVD(algorithm='arpack')))
+    steps.append(('classifier', classifier))
+
+    return Pipeline(steps)
--- preprocess_util.py
+++ preprocess_util.py
@@ -16,6 +16,8 @@ from matplotlib.pyplot import figure
 
 from sklearn.model_selection import GridSearchCV
 
+from datetime import datetime
+
 from sklearn.feature_selection import SelectKBest, chi2
 default_preprocess_settings = {
     'url_cleaner': None,
@@ -31,176 +33,6 @@ default_preprocess_settings = {
 }
 
 import matplotlib.ticker as mticker
-
-def prepare_train_data(df, y, settings = default_preprocess_settings):
-    
-    url_cleaner, preprocessor, stopwords, ngram_range, best_k, max_poly, kw, loc, hashtags, svd_n_comp = itemgetter(
-        'url_cleaner',
-        'preprocessor',
-        'stopwords',
-        'ngram_range',
-        'best_k',
-        'max_poly',
-        'kw',
-        'loc',
-        'hashtags',
-        'svd_n_comp')(settings)
-    
-    min_df = 5
-    
-    if stopwords and preprocessor:
-        stopwords = [preprocessor(word) for word in stopwords]
-    
-    c_vectorizer = CountVectorizer(min_df = min_df,
-                                   ngram_range = ngram_range,
-                                   preprocessor = preprocessor,
-                                   stop_words = stopwords)
-    kw_d_vectorizer = None
-    loc_d_vectorizer = None
-    p_features = PolynomialFeatures(max_poly)
-    s_k_best = None
-    ht_c_vectorizer = None
-    svd = None
-    
-    text = df.text
-
-    if url_cleaner:
-        text = url_cleaner.transform(df).text
-        
-    vectors = c_vectorizer.fit_transform(text)
-
-    if kw:
-        kw_d_vectorizer = DictVectorizer()
-        train_kw_dict = df[['keyword']].to_dict(orient='records')
-        train_kw_vectors = kw_d_vectorizer.fit_transform(train_kw_dict)
-        vectors = sp.hstack((vectors, train_kw_vectors))
-
-    if loc:
-        loc_d_vectorizer = DictVectorizer()
-        train_loc_dict = df[['location']].to_dict(orient='records')
-        train_loc_vectors = loc_d_vectorizer.fit_transform(train_loc_dict)
-        vectors = sp.hstack((vectors, train_loc_vectors))
-        
-    if hashtags:
-        ht_c_vectorizer = CountVectorizer(min_df = 5, analyzer = lambda x: x)
-        ht_vectors = ht_c_vectorizer.fit_transform(df.hashtags)
-        vectors = sp.hstack((vectors, ht_vectors))
-        
-    # vectors =  p_features.fit_transform(vectors)
-
-    if best_k:
-        s_k_best = SelectKBest(chi2, k=min(vectors.shape[1], best_k))
-        vectors = s_k_best.fit_transform(vectors, y)
-    
-
-    if svd_n_comp:
-        svd = TruncatedSVD(n_components=svd_n_comp, n_iter=20, random_state=42)
-        vectors = svd.fit_transform(vectors)
-        
-    return {
-        'settings': settings,
-        'transformers': {
-            'url_cleaner': url_cleaner,
-            'c_vect': c_vectorizer,
-            'kw_d_vect': kw_d_vectorizer,
-            'loc_d_vect': loc_d_vectorizer,
-            'p_feat': p_features,
-            's_k_best': s_k_best,
-            'ht_c_vect': ht_c_vectorizer,
-            'svd': svd
-        },
-        'vectors': vectors
-    }
-
-def prepare_test_vectors(df, train_data):
-    url_cleaner, c_vectorizer, kw_d_vectorizer, loc_d_vectorizer, p_features, s_k_best, ht_c_vectorizer, svd = itemgetter(
-        'url_cleaner',
-        'c_vect',
-        'kw_d_vect',
-        'loc_d_vect',
-        'p_feat',
-        's_k_best',
-        'ht_c_vect',
-        'svd')(train_data['transformers'])
-
-    text = df.text
-    if url_cleaner:
-        text = url_cleaner.transform(df).text
-        
-    vectors = c_vectorizer.transform(text)
-    
-    if kw_d_vectorizer:
-        kw_dict = df[['keyword']].to_dict(orient='records')
-        kw_vectors = kw_d_vectorizer.transform(kw_dict)
-        vectors = sp.hstack((vectors, kw_vectors))
-
-    if loc_d_vectorizer:
-        loc_dict = df[['location']].to_dict(orient='records')
-        loc_vectors = loc_d_vectorizer.transform(loc_dict)
-        vectors = sp.hstack((vectors, loc_vectors))
-        
-    if ht_c_vectorizer:
-        ht_vectors = ht_c_vectorizer.transform(df.hashtags)
-        vectors = sp.hstack((vectors, ht_vectors))
-        
-    # vectors =  p_features.transform(vectors)
-
-    if s_k_best:
-        vectors = s_k_best.transform(vectors)
-    
-    if svd:
-        vectors = svd.transform(vectors)
-        
-    return vectors
-
-def prepare_cross_val_vectors(X, y, settings=default_preprocess_settings, cv=None):
-    splits = []
-    for train_index, val_index in cv.split(X, y):
-        y_train = y.iloc[train_index]
-        train_data = prepare_train_data(X.iloc[train_index], y_train, settings=settings)
-
-        X_val = prepare_test_vectors(X.iloc[val_index], train_data)
-        y_val = y.iloc[val_index]
-        splits.append({
-            'X_train': train_data['vectors'],
-            'y_train': y_train,
-            'X_val': X_val,
-            'y_val': y_val,
-            'transformers': train_data['transformers']
-        })
-    return {
-        'splits': splits,
-        'settings': settings
-    }
-
-def my_cross_val_score(model, splits):
-    scores = []
-    for split in splits:
-        model.fit(split['X_train'], split['y_train'])
-        scores.append(f1_score(split['y_val'], model.predict(split['X_val'])))
-    return np.array(scores)
-
-def compare_models(models, vectors_combinations):
-    df = pd.DataFrame()
-    for key, data in vectors_combinations.items():
-        record_dict = {
-            'Features': key
-        }
-        for model_key, model in models.items():
-            try:
-                mean_score = my_cross_val_score(model, data['splits']).mean()
-            except Exception as e:
-                logging.warning(e)
-                mean_score = None
-            finally:
-                record_dict[model_key] = mean_score
-
-        df = pd.concat([df, pd.DataFrame([record_dict])])
-    df.set_index('Features', inplace=True)
-    return df
-
-
-
 
 def plot_score_diffs(diffs):
     fig, ax = plt.subplots(6, 1, figsize=(10,35))
@@ -235,8 +67,6 @@ def plot_score_diffs(diffs):
     plt.legend()
 
 def k_range_scores_for_pipe(pipe, k_range, cv=None, X=None, y=None):
-    print (f"Doing another pipe")
-    
     param_grid_default = {
         'poly2_k_best': [None],
         'svd': ['passthrough']
@@ -258,9 +88,7 @@ def k_range_scores_for_pipe(pipe, k_range, cv=None, X=None, y=None):
                                    cv=cv)
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        print('fitting range')
         range_results = range_k_grid_search.fit(X, y)
-        print('fitting default')
         default_results = default_grid_search.fit(X, y)
         
     return {
@@ -269,13 +97,15 @@ def k_range_scores_for_pipe(pipe, k_range, cv=None, X=None, y=None):
     }
 
 def k_range_scores(ranged_pipelines, cv=None, X=None, y=None):
-    print("whatever")
-    return {
-        k: k_range_scores_for_pipe(rp[0], rp[1], cv=cv, X=X, y=y) for k, rp in ranged_pipelines.items()
-    } 
+    result = {}
+    for k, rp in ranged_pipelines.items():
+        current_time = datetime.now().strftime("%H:%M:%S")
+        print(f"Started {k} at {current_time}")
+        result[k] = k_range_scores_for_pipe(rp[0], rp[1], cv=cv, X=X, y=y)
+    return result
+    
 
 def svd_n_range_scores_for_pipe(pipe, n_range, no_poly_k=1000, poly_2_k=1000, defaults={}, cv=None, X=None, y=None):
-    print (f"Doing another pipe")
 
     variable_defaults = {
         'No Poly, No BestK': {
@@ -306,10 +136,7 @@ def svd_n_range_scores_for_pipe(pipe, n_range, no_poly_k=1000, poly_2_k=1000, de
         param_grid_no_svd.update({
             'svd': ['passthrough']
         })
-            
-        # print(pipe)
-        # print(param_grid_ranged)
-        # pring(cv)
+
         grid_search = GridSearchCV(pipe, 
                                    param_grid = param_grid_ranged,
                                    scoring='f1',
@@ -332,10 +159,12 @@ def svd_n_range_scores_for_pipe(pipe, n_range, no_poly_k=1000, poly_2_k=1000, de
     return results
 
 def svd_n_range_scores(params_dict):
-    return {
-        key: svd_n_range_scores_for_pipe(**params) for key, params in params_dict.items()
-    }
-
+    result = {}
+    for key, params in params_dict.items():
+        current_time = datetime.now().strftime("%H:%M:%S")
+        print(f"Started {key} at {current_time}")
+        result[key] = svd_n_range_scores_for_pipe(**params)
+    return result
 
 def plot_k_range_results(all_results):
     fig, ax = plt.subplots(len(all_results), 1, figsize=(10,35))
@@ -408,4 +237,25 @@ def plot_svd__range_results(all_results):
 
         plt.xlabel('n components in TruncatedSVD')
         plt.ylabel('F1 score')
+        plt.legend()
+        
+def plot_grid_results(results, x_var, legend_var=None, log=False):
+    if legend_var is None:
+        legend_values=['dummy']
+    else:
+        legend_values=results.param_grid[legend_var]
+    for l_val in legend_values:
+        scores = results.cv_results_['mean_test_score']
+        x_values =  results.cv_results_[f'param_{x_var}'].data
+        if legend_var:
+            mask = [item[legend_var] == l_val for item in results.cv_results_['params']]
+            scores = scores[mask]
+            x_values = x_values[mask]
+        if log:
+            axes = plt.subplot()
+            axes.set_xscale('log')
+        plt.plot(x_values, scores, label=f'{legend_var} = {l_val}' if legend_var else '')
+    plt.xlabel(x_var)
+    plt.ylabel(results.scoring)
+    if legend_var is not None:
         plt.legend()
